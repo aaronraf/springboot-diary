389399ce95d913ea7fc81ed6f17b278cc0de5260
Aaron Thamin <aaron.thamin@tum.de>
2024-10-26 00:02:49 +0200

    dependency injection with component

---
+++ colours-with-component-and-friends/src/main/java/com/deviro/maven/services/impl/GermanBluePrinter.java
@@ -0,0 +1,10 @@
+package com.deviro.maven.services.impl;
+
+import com.deviro.maven.services.BluePrinter;
+
+public class GermanBluePrinter implements BluePrinter {
+    @Override
+    public String print() {
+        return "blau";
+    }
+}

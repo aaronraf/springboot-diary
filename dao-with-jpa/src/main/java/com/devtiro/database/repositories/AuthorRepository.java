e71f0897001d6a578a5adde8c102f9645b7b7fb9
Aaron Thamin <aaron.thamin@tum.de>
2024-11-29 15:30:52 +0100

    implemented @Query into AuthorRepository

--- dao-with-jpa/src/main/java/com/devtiro/database/repositories/AuthorRepository.java
+++ dao-with-jpa/src/main/java/com/devtiro/database/repositories/AuthorRepository.java
@@ -1,10 +1,16 @@
 package com.devtiro.database.repositories;
 
 import com.devtiro.database.domain.Author;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+    Iterable<Author> ageLessThan(int age);
+
+    @Query("SELECT a FROM Author a WHERE a.age > ?1")
+    Iterable<Author> findAuthorsWithAgeGreaterThan(int age);
 
 }

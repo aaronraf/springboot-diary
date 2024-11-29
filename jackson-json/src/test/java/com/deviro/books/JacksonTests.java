08713c6dfd8dd1efcdc53a1e3ae2ff6b3a9922fa
Aaron Thamin <aaron.thamin@tum.de>
2024-11-29 20:14:23 +0100

    jackson & json stuff

---
+++ jackson-json/src/test/java/com/deviro/books/JacksonTests.java
@@ -0,0 +1,42 @@
+package com.deviro.books;
+
+import com.deviro.books.domain.Book;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class JacksonTests {
+
+    @Test
+    public void testThatObjectMapperCanCreateJsonFromObject() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Book book = Book.builder()
+                .isbn("978-0-13-478627-5")
+                .title("The Enigma of Eternity")
+                .author("Aria Montgomery")
+                .yearPublished("2005")
+                .build();
+
+        String result = objectMapper.writeValueAsString(book);
+        assertThat(result).isEqualTo("{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria Montgomery\",\"year\":\"2005\"}");
+    }
+
+    @Test
+    public void testThatObjectMappedCanCreateJavaObjectFromJsonObject() throws JsonProcessingException {
+        Book book = Book.builder()
+                .isbn("978-0-13-478627-5")
+                .title("The Enigma of Eternity")
+                .author("Aria Montgomery")
+                .yearPublished("2005")
+                .build();
+        String json = "{\"foo\":\"bar\",\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria Montgomery\",\"year\":\"2005\"}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Book result = objectMapper.readValue(json, Book.class);
+        assertThat(result).isEqualTo(book);
+    }
+
+}
